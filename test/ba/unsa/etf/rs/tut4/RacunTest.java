30af31aeb0782a7cfa15da00fa79203ffde69db9
mDrinjak <mdrinjak1@etf.unsa.ba>
2020-03-27 19:48:37 +0100

    Treći zadatak.

--- test/ba/unsa/etf/rs/tut4/RacunTest.java
+++ test/ba/unsa/etf/rs/tut4/RacunTest.java
@@ -1,10 +1,10 @@
-/*package ba.unsa.etf.rs.tut4;
+package ba.unsa.etf.rs.tut4;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RacunTest {}
+class RacunTest {
 
   @Test
     void test1() {
@@ -19,4 +19,4 @@ class RacunTest {}
         Racun r = new Racun();
         assertEquals(0, r.ukupanIznos());
     }
-}*/
+}
